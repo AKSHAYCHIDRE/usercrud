8887aa5155ea9e15e073fc982746564e0b4111e5
AKSHAYCHIDRE <akshaychidre0@gmail.com>
2023-07-27 16:59:51 +0530

    updated the user curd operation code

--- src/components/read.jsx
+++ src/components/read.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 
 function Read() {
   const [data, setData] = useState([]);
-  const [tableDark, setTableDark]= useState('');
+  const [tableDark, setTableDark] = useState("");
+  const [inputText, setInputText] = useState("");
 
   function getData() {
     axios
@@ -32,13 +33,22 @@ function Read() {
     getData();
   }, []);
 
+  function inputHandler(e) {
+    setInputText(e.target.value?.toLowerCase());
+  }
+
   return (
     <>
-        <div className="form-check form-switch">
-            <input class="form-check-input" type="checkbox" role="switch" onClick={()=> {
-              setTableDark(tableDark === 'table-dark' ? '' : 'table-dark')
-            }} />
-        </div>
+      <div className="form-check form-switch">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          role="switch"
+          onClick={() => {
+            setTableDark(tableDark === "table-dark" ? "" : "table-dark");
+          }}
+        />
+      </div>
       <div className="d-flex justify-content-between align-items-center mb-3">
         <h2>List of users</h2>
         <Link to="/">
@@ -47,50 +57,75 @@ function Read() {
           </button>
         </Link>
       </div>
-      <table className={`table p-3 ${tableDark}`}>
+      <div className="col-md-4">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search Here..."
+          onChange={inputHandler}
+        />
+      </div>
+      <table className={`table p-3 my-3 ${tableDark}`}>
         <thead>
           <tr>
-            <th className="p-3" scope="col">#</th>
-            <th className="p-3" scope="col">Name</th>
-            <th className="p-3" scope="col">Email</th>
+            <th className="p-3" scope="col">
+              #
+            </th>
+            <th className="p-3" scope="col">
+              Name
+            </th>
+            <th className="p-3" scope="col">
+              Email
+            </th>
             <th className="p-3" scope="col"></th>
             <th className="p-3" scope="col"></th>
           </tr>
         </thead>
         <tbody>
-          {data.map((user) => (
-            <>
-              <tr>
-                <th className="p-3" scope="row" key={user.id}>
-                  {user.id}
-                </th>
-                <td className="p-3">{user.name}</td>
-                <td className="p-3">{user.email}</td>
-                <td className="p-3">
-                  <Link to="/update">
+          {data
+            .filter((el) => {
+              if (el === "") {
+                return <h2>No Result Found</h2>;
+              } else {
+                return (
+                  el.name?.toLowerCase().includes(inputText) ||
+                  el.email?.toLowerCase().includes(inputText)
+                );
+              }
+            })
+            .map((user) => (
+              <>
+                <tr>
+                  <th className="p-3" scope="row" key={user.id}>
+                    {user.id}
+                  </th>
+                  <td className="p-3">{user.name}</td>
+                  <td className="p-3">{user.email}</td>
+                  <td className="p-3">
+                    <Link to="/update">
+                      <button
+                        type="button"
+                        onClick={() =>
+                          setToLoacalStorage(user.id, user.name, user.email)
+                        }
+                        className="btn btn-primary"
+                      >
+                        Edit
+                      </button>
+                    </Link>
+                  </td>
+                  <td className="p-3">
                     <button
                       type="button"
-                      onClick={() =>
-                        setToLoacalStorage(user.id, user.name, user.email)
-                      }
-                      className="btn btn-primary"
+                      onClick={() => handleDelete(user.id)}
+                      className="btn btn-danger"
                     >
-                      Edit
+                      Delete
                     </button>
-                  </Link>
-                </td>
-                <td className="p-3">
-                  <button
-                    type="button"
-                    onClick={() => handleDelete(user.id)}
-                    className="btn btn-danger"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            </>
-          ))}
+                  </td>
+                </tr>
+              </>
+            ))}
         </tbody>
       </table>
     </>
